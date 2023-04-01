91b9e03211545ffeb73b72d454b2dd59da4bfd80
cwgoh <129590817+cwgoh@users.noreply.github.com>
2023-04-01 23:24:57 +0200

    Update q1.sql

--- transform/models/submission/q1.sql
+++ transform/models/submission/q1.sql
@@ -1,1 +1,17 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
 
+WITH total_gmv AS (
+  SELECT  
+    country_name,
+    ROUND(SUM(gmv_local), 2) total_gmv
+
+  FROM
+    orders o
+
+  GROUP BY
+    o.country_name;
+)
