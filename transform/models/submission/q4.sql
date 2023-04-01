683e4d3ecff06309d607b915a948d50bdfc43ff1
cwgoh <129590817+cwgoh@users.noreply.github.com>
2023-04-01 23:39:26 +0200

    Create q4.sql

---
+++ transform/models/submission/q4.sql
@@ -0,0 +1,38 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+WITH top_vendors AS (
+SELECT
+  year,
+  v.country_name,
+  v.vendor_name,
+  total_gmv
+
+FROM (
+  SELECT
+    o.vendor_id,
+    o.country_name,
+    EXTRACT(YEAR FROM o.date_local) AS year,
+    ROUND(SUM(o.gmv_local), 2) AS total_gmv,
+    ROW_NUMBER() OVER (PARTITION BY o.country_name, year ORDER BY SUM(o.gmv_local) DESC) rn
+    
+  FROM
+    `orders` o
+  
+  GROUP BY
+    o.vendor_id,
+    o.country_name,
+    year
+) `orders`, `vendors` v
+
+WHERE
+  rn < 3 AND vendor_id = v.id
+
+ORDER BY
+  year,
+  v.country_name,
+  rn;
+)
